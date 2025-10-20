2b73202d27339d085d3144250ac6f8ef1226e02c
Ezeofor PraiseGod <senitenttechnology@gmail.com>
2025-10-20 18:06:22 +0100

    implemented search funtionality and moved components folder to the root folder

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,27 @@
-const page = () => {
+import SearchForm from '@/components/SearchForm';
+
+const Home = async ({
+	searchParams,
+}: {
+	searchParams: Promise<{ query?: string }>;
+}) => {
+	const query = (await searchParams).query;
+
 	return (
 		<>
-			<h1 className='text-3xl'>Home</h1>
+			<section className='pink_container pattern'>
+				<h1 className='heading'>
+					Pitch your startup <br /> Connect with Entrepreneurs
+				</h1>
+
+				<p className='sub-heading max-w-3xl'>
+					Submit ideas. Vote on Pitches, and Get Noticed in Vitual Competitions.
+				</p>
+
+				<SearchForm query={query} />
+			</section>
 		</>
 	);
 };
 
-export default page;
+export default Home;
