a37ca7447a0eba0bb18b44e19d96433941878dd7
Ezeofor PraiseGod <senitenttechnology@gmail.com>
2025-10-25 13:37:41 +0100

    implemented sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
 
 const Home = async ({
 	searchParams,
@@ -6,6 +7,20 @@ const Home = async ({
 	searchParams: Promise<{ query?: string }>;
 }) => {
 	const query = (await searchParams).query;
+
+	const posts = [
+		{
+			_createdAt: new Date(),
+			views: 55,
+			author: { _id: 1, name: 'Praise Ezeofor' },
+			_id: 1,
+			description: 'This is a description',
+			image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRByBdJDQihtV41mKbZ4CiQrXTc8ptphn968A&s',
+			category: 'Robots',
+			title: 'We Robots',
+		},
+	];
 
 	return (
 		<>
@@ -19,6 +34,22 @@ const Home = async ({
 				</p>
 
 				<SearchForm query={query} />
+			</section>
+
+			<section className='section_container'>
+				<p className='text-30-semibold'>
+					{query ? `Search results for "${query}"` : 'All startups'}
+				</p>
+
+				<ul className='mt-7 card_grid'>
+					{posts?.length > 0 ? (
+						posts.map((post: StartupCardType, index: number) => (
+							<StartupCard key={post?._id} post={post} />
+						))
+					) : (
+						<p className='no-results'>No Startups found</p>
+					)}
+				</ul>
 			</section>
 		</>
 	);
