b517c1cb4d63178a030c53f6d9712964646511c3
Ezeofor PraiseGod <senitenttechnology@gmail.com>
2025-10-25 16:41:52 +0100

    implemented sanity generation of types for typescript

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
 import SearchForm from '@/components/SearchForm';
 import StartupCard from '@/components/StartupCard';
+import { client } from '@/sanity/lib/client';
+import { STARTUPS_QUERY } from '@/sanity/lib/queries';
+
+import { StartupTypeCard } from '@/components/StartupCard';
 
 const Home = async ({
 	searchParams,
@@ -8,19 +12,7 @@ const Home = async ({
 }) => {
 	const query = (await searchParams).query;
 
-	const posts = [
-		{
-			_createdAt: new Date(),
-			views: 55,
-			author: { _id: 1, name: 'Praise Ezeofor' },
-			_id: 1,
-			description: 'This is a description',
-			image:
-				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRByBdJDQihtV41mKbZ4CiQrXTc8ptphn968A&s',
-			category: 'Robots',
-			title: 'We Robots',
-		},
-	];
+	const posts = await client.fetch(STARTUPS_QUERY);
 
 	return (
 		<>
@@ -43,7 +35,7 @@ const Home = async ({
 
 				<ul className='mt-7 card_grid'>
 					{posts?.length > 0 ? (
-						posts.map((post: StartupCardType, index: number) => (
+						posts.map((post: StartupTypeCard) => (
 							<StartupCard key={post?._id} post={post} />
 						))
 					) : (
